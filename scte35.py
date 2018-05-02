a305ef4675f9a79442cef35b5fe9fe5361491226
atoztoa <abheeshg@gmail.com>
2018-05-02 10:06:55 -0600

    Converted to JSON based output.

--- scte35.py
+++ scte35.py
@@ -31,194 +31,266 @@ THE SOFTWARE.
 
 import bitstring
 from datetime import timedelta
+from enum import IntEnum
+
+class SpliceDescriptor(IntEnum):
+  AVAIL_DESCRIPTOR = 0
+  DTMF_DESCRIPTOR = 1
+  SEGMENTATION_DESCRIPTOR = 2
+  TIME_DESCRIPTOR = 3
 
 
-class MixinDictRepr(object):
-    def __repr__(self, *args, **kwargs):
-        return repr(self.__dict__)
+class MPEG_Time(int):
+  """ Relative time represented by 90kHz clock """
 
-class SCTE35_ParseException(Exception):
-    pass
+  @property
+  def seconds(self):
+    return self / 90000.0
 
-class SCTE35_SpliceInfoSectionException(Exception):
-    pass
+  @property
+  def timedelta(self):
+    return timedelta(seconds=self.seconds)
 
-class SCTE35_SpliceInfoSection(MixinDictRepr):
-    
-    def __init__(self, table_id):
-        if table_id != 0xfc:
-            raise SCTE35_SpliceInfoSectionException("%d valid. Should be 0xfc" %
-                                                    table_id)
-
-class MPEG_Time(long):
-    """ Relative time represented by 90kHz clock """
-    
-    @property
-    def seconds(self):
-        return self / 90000.0
-    
-    @property
-    def timedelta(self):
-        return timedelta(seconds=self.seconds)
-    
-    def __repr__(self, *args, **kwargs):
-        return "%d (seconds: %f, time: %s)" % (self, self.seconds, self.timedelta)
-
-class SCTE35_SpliceInsert(MixinDictRepr):
-    
-    class Splice_Time(MixinDictRepr):
-        def __init__(self):
-            self.time_specified_flag =  None
-            self.pts_time = None
-
-    class Component(MixinDictRepr):
-        def __init__(self, tag):
-            self.tag = tag
-            self.splice_time = None
-            
-    class BreakDuration(MixinDictRepr):
-        def __init__(self):
-            self.auto_return = None
-            self.duration = None
-    
-    def __init__(self, splice_id):
-        self.splice_id = splice_id
-        self.components = []
-        
-    
-class SCTE35_SpliceDescriptor(MixinDictRepr):
-    pass
+  def __repr__(self, *args, **kwargs):
+    return "%d (seconds: %f, time: %s)" % (self, self.seconds, self.timedelta)
 
 
 class SCTE35_Parser(object):
-    
-    def parse(self, input_bytes):
-        input_bitarray = bitstring.BitString(bytes=input_bytes)
+  def parse(self, input_bytes):
+    print("---> parse")
 
-        table_id = input_bitarray.read("uint:8")
-        splice_info_section = SCTE35_SpliceInfoSection(table_id)
-        
-        splice_info_section.section_syntax_indicator = input_bitarray.read("bool")
-        splice_info_section.private = input_bitarray.read("bool")
-        #private
-        input_bitarray.pos += 2
-        splice_info_section.section_length = input_bitarray.read("uint:12")
-        splice_info_section.protocol_version = input_bitarray.read("uint:8")
-        splice_info_section.encrypted_packet = input_bitarray.read("bool")
-        splice_info_section.encryption_algorithm = input_bitarray.read("uint:6")
-        splice_info_section.pts_adjustment = input_bitarray.read("uint:33")
-        splice_info_section.cw_index = input_bitarray.read("uint:8")
-        splice_info_section.tier = input_bitarray.read("hex:12")
-        splice_info_section.splice_command_length = input_bitarray.read("uint:12")
+    input_bitarray = bitstring.BitString(bytes=input_bytes)
 
-        splice_info_section.splice_command_type = input_bitarray.read("uint:8")
-        
-        # splice command type parsing
-        if splice_info_section.splice_command_type == 5:
-            splice_info_section.splice_command = self.__parse_splice_insert(input_bitarray)
-        else:
-            raise SCTE35_SpliceInfoSectionException("splice_command_type: %d not yet supported" % splice_info_section.splice_command_type)
+    table_id = input_bitarray.read("uint:8")
 
-        splice_info_section.splice_descriptor_loop_length = input_bitarray.read("uint:16")
-        splice_info_section.splice_descriptors = None
-        
-        if splice_info_section.splice_descriptor_loop_length > 0:
-            splice_info_section.splice_descriptors = \
-             self.__parse_splice_descriptors(input_bitarray, 
-                                             splice_info_section.splice_descriptor_loop_length)
-        
-        return splice_info_section
-        
-    def __parse_splice_time(self, bitarray):
-        splice_time = SCTE35_SpliceInsert.Splice_Time()
-        splice_time.time_specified_flag = bitarray.read("bool")
-        
-        if splice_time.time_specified_flag:
-            # Reserved for 6 bits
-            bitarray.pos += 6
-            splice_time.pts_time = MPEG_Time(bitarray.read("uint:33"))
-        else:
-            bitarray.pos += 7
-            
-        return splice_time
-            
-    def __parse_break_duration(self, bitarray):
-        break_duration = SCTE35_SpliceInsert.BreakDuration()
-        break_duration.auto_return = bitarray.read("bool")
-        bitarray.pos += 6
-        break_duration.duration = MPEG_Time(bitarray.read("uint:33"))
-        return break_duration
-    
-    def __parse_splice_insert(self, bitarray):
-        splice_event_id = bitarray.read("uint:32")
-        ssi = SCTE35_SpliceInsert(splice_event_id)
-        
-        ssi.splice_event_cancel_indicator = bitarray.read("bool")
-        bitarray.pos += 7
-        
-        if not ssi.splice_event_cancel_indicator:
-            ssi.out_of_network_indicator = bitarray.read("bool")
-            ssi.program_splice_flag = bitarray.read("bool")
-            ssi.duration_flag = bitarray.read("bool")
-            ssi.splice_immediate_flag = bitarray.read("bool")
-            # Next 4 bits are reserved
-            bitarray.pos += 4
-            
-            if ssi.program_splice_flag and not ssi.splice_immediate_flag:
-                ssi.splice_time = self.__parse_splice_time(bitarray)
-                
-            if not ssi.program_splice_flag:
-                ssi.component_count = bitarray.read("uint:8")
-                
-                for i in xrange(0, ssi.component_count):
-                    component = SCTE35_SpliceInsert.Component(bitarray.read("uint:8"))
-                    if ssi.splice_immediate_flag:
-                        component.splice_time = self.__parse_splice_time(bitarray)
-                    ssi.components.append(component)
-        
-        
-            if ssi.duration_flag:
-                ssi.break_duration = self.__parse_break_duration(bitarray)
-                
-            ssi.unique_program_id = bitarray.read("uint:16")
-            ssi.avail_num = bitarray.read("uint:8")
-            ssi.avails_expected = bitarray.read("uint:8")
-            return ssi
-        
-    def __parse_splice_descriptors(self, bitarray, length):
-        
-        results = []
-        
-        while length > 6:        
-            splice_desc = SCTE35_SpliceDescriptor();
-            
-            splice_desc.splice_descriptor_tag = bitarray.read("uint:8")
-            splice_desc.descriptor_length = bitarray.read("uint:8")
-            splice_desc.identifier = bitarray.read("uint:32")
-    
-            length -= 6
-    
-            results.append( splice_desc )
-        return results
+    if table_id != 0xfc:
+      raise Exception("table_id %d invalid. Should be 0xfc" % table_id)
+
+    splice_info_section = {}
+
+    splice_info_section["section_syntax_indicator"] = input_bitarray.read("bool")
+    splice_info_section["private_indicator"] = input_bitarray.read("bool")
+
+    input_bitarray.pos += 2
+    splice_info_section["section_length"] = input_bitarray.read("uint:12")
+    splice_info_section["protocol_version"] = input_bitarray.read("uint:8")
+    splice_info_section["encrypted_packet"] = input_bitarray.read("bool")
+    splice_info_section["encryption_algorithm"] = input_bitarray.read("uint:6")
+    splice_info_section["pts_adjustment"] = input_bitarray.read("uint:33")
+    splice_info_section["cw_index"] = input_bitarray.read("uint:8")
+    splice_info_section["tier"] = input_bitarray.read("hex:12")
+    splice_info_section["splice_command_length"] = input_bitarray.read("uint:12")
+
+    splice_info_section["splice_command_type"] = input_bitarray.read("uint:8")
+
+    # splice command type parsing
+    if splice_info_section["splice_command_type"] == 5:
+      splice_info_section["splice_command"] = self.__parse_splice_insert(input_bitarray)
+    elif splice_info_section["splice_command_type"] == 6:
+      splice_info_section["splice_command"] = self.__parse_time_signal(input_bitarray)
+    else:
+      raise Exception("splice_command_type: %d not yet supported" % splice_info_section["splice_command_type"])
+
+    # Total bytes in splice_descriptors
+    splice_info_section["splice_descriptor_loop_length"] = input_bitarray.read("uint:16")
+    splice_info_section["splice_descriptors"] = None
+
+    if splice_info_section["splice_descriptor_loop_length"] > 0:
+      splice_info_section["splice_descriptors"] = (
+       self.__parse_splice_descriptors(input_bitarray,
+                       splice_info_section["splice_descriptor_loop_length"]))
+
+    return splice_info_section
+
+  def __parse_splice_time(self, bitarray):
+    print("---> parse_splice_time")
+
+    splice_time = {}
+    splice_time["time_specified_flag"] = bitarray.read("bool")
+
+    if splice_time["time_specified_flag"]:
+      # Reserved for 6 bits
+      bitarray.pos += 6
+      splice_time["pts_time"] = MPEG_Time(bitarray.read("uint:33"))
+    else:
+      bitarray.pos += 7
+
+    return splice_time
+
+  def __parse_break_duration(self, bitarray):
+    print("---> parse_break_duration")
+
+    break_duration = {}
+    break_duration["auto_return"] = bitarray.read("bool")
+    bitarray.pos += 6
+    break_duration["duration"] = MPEG_Time(bitarray.read("uint:33"))
+    return break_duration
+
+  def __parse_splice_insert(self, bitarray):
+    print("---> parse_splice_insert")
+
+    splice_event_id = bitarray.read("uint:32")
+    ssi = {}
+
+    ssi["splice_id"] = splice_event_id
+    ssi["components"] = []
+
+    ssi["splice_event_cancel_indicator"] = bitarray.read("bool")
+    bitarray.pos += 7
+
+    if not ssi["splice_event_cancel_indicator"]:
+      ssi["out_of_network_indicator"] = bitarray.read("bool")
+      ssi["program_splice_flag"] = bitarray.read("bool")
+      ssi["duration_flag"] = bitarray.read("bool")
+      ssi["splice_immediate_flag"] = bitarray.read("bool")
+      # Next 4 bits are reserved
+      bitarray.pos += 4
+
+      if ssi["program_splice_flag"] and not ssi["splice_immediate_flag"]:
+        ssi["splice_time"] = self.__parse_splice_time(bitarray)
+
+      if not ssi["program_splice_flag"]:
+        ssi["component_count"] = bitarray.read("uint:8")
+
+        for i in xrange(0, ssi["component_count"]):
+          component = {}
+
+          component["tag"] = bitarray.read("uint:8")
+          component["splice_time"] = None
+
+
+          if ssi["splice_immediate_flag"]:
+            component["splice_time"] = self.__parse_splice_time(bitarray)
+          ssi["components"].append(component)
+
+
+      if ssi["duration_flag"]:
+        ssi["break_duration"] = self.__parse_break_duration(bitarray)
+
+      ssi["unique_program_id"] = bitarray.read("uint:16")
+      ssi["avail_num"] = bitarray.read("uint:8")
+      ssi["avails_expected"] = bitarray.read("uint:8")
+      return ssi
+
+  def __parse_time_signal(self, bitarray):
+    print("---> parse_time_signal")
+
+    ssi = {}
+
+    ssi["splice_time"] = self.__parse_splice_time(bitarray)
+    return ssi
+
+  def __parse_segmentation_descriptor(self, bitarray, tag, length):
+    segmentation_descriptor = {}
+
+    segmentation_descriptor["segmentation_descriptor_tag"] = tag
+    segmentation_descriptor["descriptor_length"] = length
+
+    segmentation_descriptor["identifier"] = bitarray.read("uint:32")
+
+    descriptor_data_length = segmentation_descriptor["descriptor_length"] - 4
+
+    segmentation_descriptor["segmentation_event_id"] = bitarray.read("uint:32")
+    segmentation_descriptor["segmentation_event_cancel_indicator"] = bitarray.read("bool")
+
+    # Reserved bits
+    bitarray.pos += 7
+
+    if not segmentation_descriptor["segmentation_event_cancel_indicator"]:
+      segmentation_descriptor["program_segmentation_flag"] = bitarray.read("bool")
+      segmentation_descriptor["segmentation_duration_flag"] = bitarray.read("bool")
+      segmentation_descriptor["delivery_not_restricted_flag"] = bitarray.read("bool")
+
+      if not segmentation_descriptor["delivery_not_restricted_flag"]:
+        segmentation_descriptor["web_delivery_allowed_flag"] = bitarray.read("bool")
+        segmentation_descriptor["no_regional_blackout_flag"] = bitarray.read("bool")
+        segmentation_descriptor["archive_allowed_flag"] = bitarray.read("bool")
+        segmentation_descriptor["device_restrictions"] = bitarray.read("uint:2")
+      else:
+        bitarray.pos += 5
+
+      if not segmentation_descriptor["program_segmentation_flag"]:
+        segmentation_descriptor["component_count"] = bitarray.read("uint:8")
+        segmentation_descriptor["components"] = []
+
+        for i in range(segmentation_descriptor["component_count"]):
+          component = {}
+          component["component_tag"] = bitarray.read("uint:8")
+          bitarray.pos += 7
+          component["pts_offset"] = bitarray.read("uint:33")
+
+          segmentation_descriptor["components"].append(component)
+
+      if segmentation_descriptor["segmentation_duration_flag"]:
+        segmentation_descriptor["segmentation_duration"] = bitarray.read("uint:40")
+
+      segmentation_descriptor["segmentation_upid_type"] = bitarray.read(8).hex
+      segmentation_descriptor["segmentation_upid_length"] = bitarray.read("uint:8")
+
+
+
+      segmentation_descriptor["segmentation_upid"] = self.__parse_segmentation_upid(
+        bitarray,
+        int(segmentation_descriptor["segmentation_upid_type"], 16),
+        segmentation_descriptor["segmentation_upid_length"]
+      )
+
+      segmentation_descriptor["segmentation_type_id"] = bitarray.read(8).hex
+      segmentation_descriptor["segment_num"] = bitarray.read("uint:8")
+      segmentation_descriptor["segments_expected"] = bitarray.read("uint:8")
+
+      if segmentation_descriptor["segmentation_type_id"] in ["34", "36"]:
+        segmentation_descriptor["sub_segment_num"] = bitarray.read("uint:8")
+        segmentation_descriptor["sub_segments_expected"] = bitarray.read("uint:8")
+
+    return segmentation_descriptor
+
+  def __parse_segmentation_upid(self, bitarray, upid_type, length):
+    # Deprecated
+    if upid_type in [1, 2, 5]:
+      raise Exception("Deprecated upid_type: %d" % upid_type)
+
+    return bitarray.read(length * 8).hex
+
+
+  def __parse_splice_descriptors(self, bitarray, length):
+    print("---> parse_splice_descriptors")
+
+
+    results = []
+
+    while length:
+      splice_descriptor_tag = bitarray.read("uint:8")
+      descriptor_length = bitarray.read("uint:8")
+
+      length -= descriptor_length + 2
+
+      if splice_descriptor_tag == SpliceDescriptor.SEGMENTATION_DESCRIPTOR:
+        splice_descriptor = self.__parse_segmentation_descriptor(bitarray, splice_descriptor_tag, descriptor_length)
+      else:
+        raise Exception("Invalid or unsupported splice_descriptor: %d" % splice_descriptor_tag)
+
+      results.append(splice_descriptor)
+    return results
 
 if __name__ == "__main__":
-    import base64
-    import argparse
-    import pprint
-    
-    description = """ Parse SCTE-35 markers from Base64 Strings.
-Example String: "/DAlAAAAAAAAAP/wFAUAAAABf+/+LRQrAP4BI9MIAAEBAQAAfxV6SQ==" """
-    
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('base64_scte35', metavar='SCTE35_Marker',
-                   help='Base64 encoded SCTE-35 marker')
-    
-    args = parser.parse_args()
-    
-    myinput = base64.standard_b64decode(args.base64_scte35)
-    
-    splice_info_section = SCTE35_Parser().parse(myinput)
+  import base64
+  import argparse
+  import json
 
-    print "Parsing Complete"
-    
-    pprint.pprint(vars(splice_info_section))
+  description = """ Parse SCTE-35 markers from Base64 Strings.
+Example String: "/DAlAAAAAAAAAP/wFAUAAAABf+/+LRQrAP4BI9MIAAEBAQAAfxV6SQ==" """
+
+  parser = argparse.ArgumentParser(description=description)
+  parser.add_argument('base64_scte35', metavar='SCTE35_Marker',
+           help='Base64 encoded SCTE-35 marker')
+
+  args = parser.parse_args()
+
+  input_bytes = base64.standard_b64decode(args.base64_scte35)
+
+  splice_info_section = SCTE35_Parser().parse(input_bytes)
+
+  print("Parsing Complete")
+
+  print(json.dumps(splice_info_section, indent=2))
